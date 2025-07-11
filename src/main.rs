98c431bd8f0a9ba415ee7c3c1b5d828a4f4ac3d7
Arijit Gogoi <91205677+arijit-gogoi@users.noreply.github.com>
2025-07-12 02:18:13 +0530

    accept dates while adding entry

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use chrono::{Local, NaiveDate};
 use clap::{Arg, Command};
+use csv::{ReaderBuilder, WriterBuilder};
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 use std::{
@@ -10,6 +12,7 @@ use std::{
 // Define Expense struct
 #[derive(Serialize, Deserialize, Debug)]
 struct Expense {
+    date: NaiveDate,
     category: String,
     amount: f64,
     description: String,
@@ -43,7 +46,7 @@ impl ExpenseTracker {
         self.expenses.iter().map(|e| e.amount).sum()
     }
 
-    fn save_to_file(&self, filename: &str) -> io::Result<()> {
+    fn save_to_json(&self, filename: &str) -> io::Result<()> {
         let file = OpenOptions::new()
             .create(true)
             .write(true)
@@ -53,12 +56,12 @@ impl ExpenseTracker {
         Ok(())
     }
 
-    fn load_from_file(filename: &str) -> Result<ExpenseTracker> {
+    fn load_from_json(filename: &str) -> Result<ExpenseTracker> {
         let path = Path::new(filename);
         if !path.exists() {
             return Ok(ExpenseTracker::new());
         }
-        let file = File::open(filename).expect("file exists");
+        let file = File::open(filename).expect("File should exist");
         let tracker: ExpenseTracker = serde_json::from_reader(file)?;
         Ok(tracker)
     }
@@ -88,6 +91,12 @@ fn main() {
                     Arg::new("description")
                         .required(true)
                         .help("A description for the expense"),
+                )
+                .arg(
+                    Arg::new("date")
+                        .required(false)
+                        .help("The date of expense")
+                        .value_parser(clap::value_parser!(String)),
                 ),
         )
         .subcommand(
@@ -115,28 +124,36 @@ fn main() {
         .get_matches();
 
     let filename = "expenses.json";
-    let mut tracker = ExpenseTracker::load_from_file(filename).unwrap_or_else(|err| {
+    let mut tracker = ExpenseTracker::load_from_json(filename).unwrap_or_else(|err| {
         eprintln!("Error loading data: {}", err);
         std::process::exit(1);
     });
 
     match matches.subcommand() {
         Some(("add", sub_matches)) => {
+            let date_string_opt = sub_matches.get_one::<String>("date");
+            let date_string = match date_string_opt {
+                Some(d) => d,
+                None => &Local::now().date_naive().to_string(),
+            };
+            let date = NaiveDate::parse_from_str(date_string, "%Y-%m-%d")
+                .expect("Should be correctly formatted: %Y-%m-%d (for example, 2025-12-31)");
             let category = sub_matches.get_one::<String>("category").unwrap();
             let amount: f64 = *sub_matches
                 .try_get_one::<f64>("amount")
                 .expect("amount should be a number")
-                .expect("amount must be a float");
+                .expect("amount should be a float");
             let description = sub_matches.get_one::<String>("description").unwrap();
 
             let expense = Expense {
-                category: category.clone(),
+                date,
                 amount,
+                category: category.clone(),
                 description: description.clone(),
             };
 
             tracker.add_expense(expense);
-            if let Err(err) = tracker.save_to_file(filename) {
+            if let Err(err) = tracker.save_to_json(filename) {
                 eprintln!("Error saving data: {}", err);
                 std::process::exit(1);
             }
@@ -147,7 +164,7 @@ fn main() {
             let row_number = sub_matches
                 .try_get_one::<usize>("row_number")
                 .expect("row number should be a number")
-                .expect("Enter some number");
+                .expect("some number should be given");
 
             let length = &tracker.expenses.len();
             if row_number > length {
@@ -155,7 +172,7 @@ fn main() {
             }
             tracker.delete_expense(*row_number);
 
-            if let Err(err) = tracker.save_to_file(filename) {
+            if let Err(err) = tracker.save_to_json(filename) {
                 eprintln!("Error deleting: {}", err);
                 std::process::exit(1);
             }
@@ -166,17 +183,18 @@ fn main() {
             } else {
                 for (i, expense) in tracker.expenses.iter().enumerate() {
                     println!(
-                        "{}. Category: {}, Amount: ₹{}, Description: {}",
+                        "{}. Date: {}, Category: {}, Amount: ₹{}, Description: {}",
                         i + 1,
+                        expense.date,
                         expense.category,
                         expense.amount,
-                        expense.description
+                        expense.description,
                     );
                 }
             }
         }
         Some(("total", _)) => {
-            println!("Total expenses: ${:.2}", tracker.total_expenses());
+            println!("Total expenses: ₹{:.2}", tracker.total_expenses());
         }
         _ => {
             eprintln!("Invalid command. Use 'add', 'list', 'delete', or 'total'.");
